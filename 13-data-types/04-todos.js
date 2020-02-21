c3271c5aa5ba74e4c5497c292abc19a0de95c3b1
IceOnFire <antony.mistretta@gmail.com>
2020-02-21 14:13:15 +0100

    Add todo creation logic

--- 13-data-types/04-todos.js
+++ 13-data-types/04-todos.js
@@ -22,7 +22,34 @@ render()
 document.querySelector('form').onsubmit = function(event) {
   event.preventDefault()
 
-  let todo = { id: 4, text: 'Eat your vegetables' }
-  todos.push(todo)
+  let id = generateId()
+  let text = document.querySelector('input').value
+
+  todos.push({ id, text })
+
+  document.querySelector('input').value = ''
   render()
+}
+
+// function generateId() {
+//   if (todos.length === 0) {
+//     return 1
+//   }
+
+//   let lastTodo = todos[todos.length - 1]
+//   let lastId = lastTodo.id
+//   let newId = lastId + 1
+//   return newId
+// }
+
+// function generateId() {
+//   if (todos.length === 0) {
+//     return 1
+//   } else {
+//     return todos[todos.length - 1].id + 1
+//   }
+// }
+
+function generateId() {
+  return !todos.length ? 1 : todos[todos.length - 1].id + 1
 }
