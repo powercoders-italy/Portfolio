fbc65cb4472a48c666d1ea54d3855feeb5a163b0
IceOnFire <antony.mistretta@gmail.com>
2020-03-03 08:57:27 +0100

    Add some more meaningful comments

--- 17-async/01-async.js
+++ 17-async/01-async.js
@@ -1,20 +1,25 @@
-let id = setInterval(() => console.log('Yo'), 1000)
+/* global $ */
 
-console.log('Hello world!')
+/**
+ * Asynchronous behavior: execution is not stopped by an asynchronous function
+ */
+let id = setInterval(() => console.log('This comes next'), 1000)
+console.log('This comes first')
+// you can even stop the async function on the run
+clearInterval(id)
 
-// clearInterval(id)
-
-// callback hell
+/**
+ * Using only callback functions leads to a "callback hell"
+ */
 $.get(
   'https://swapi.co/api/people/',
   function success(response) {
-    let id = response.data[0].id
+    let id = response.data.results[0].id
     $.get(
       `https://swapi.co/api/people/${id}`,
       function success(response) {
+        // this is the last statement that will be executed
         console.log(response.data)
-
-        // in here
       },
       function failure(error) {
         console.log(error.message)
@@ -26,13 +31,17 @@ $.get(
   }
 )
 
-// thenable object
+// statemements here happen before anything inside the $.get
+
+/**
+ * A promise is a "thenable" object
+ */
 $.get('https://swapi.co/api/people/')
-  // resolve
+  // it can resolve
   .then(response => {
     let id = response.data.results[0].id
     return $.get(`https://swapi.co/api/people/${id}`)
   })
   .then(response => console.log(response.data))
-  // reject
+  // it can reject
   .catch(error => console.error(error))
