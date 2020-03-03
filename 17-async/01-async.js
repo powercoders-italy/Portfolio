c4f6b1ff969349b9141c4a886b58c1937296d208
IceOnFire <antony.mistretta@gmail.com>
2020-03-03 18:01:35 +0100

    Show async/await

--- 17-async/01-async.js
+++ 17-async/01-async.js
@@ -1,4 +1,4 @@
-/* global $ */
+/* global $, fs, collection */
 
 /**
  * Asynchronous behavior: execution is not stopped by an asynchronous function
@@ -52,3 +52,100 @@ fetch('https://swapi.co/api/people/')
   .then(body1 => fetch(body1.results[0].url))
   .then(response2 => response2.json())
   .then(body2 => console.log(body2))
+
+let answer = 52
+
+new Promise((resolve, reject) => {
+  if (answer > 50) {
+    reject(new Error(`${answer} should be less than 50`))
+  } else {
+    resolve(`The number chosen was ${answer}`)
+  }
+})
+  .then(number => console.log(number))
+  .catch(err => console.error('Oops, something went wrong:', err))
+
+/**
+ * "Promisify" an asynchronous function
+ */
+let promise = new Promise(resolve => {
+  setTimeout(() => {
+    resolve('A second has passed')
+  }, 1000)
+})
+promise.then(result => console.log(result))
+
+new Promise((resolve, reject) => {
+  fs.readFile('myfile.txt', (err, data) => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(data)
+    }
+  })
+})
+  .then(file => console.log(file))
+  .catch(err => console.error(err))
+
+let myReadFile = util.promisify(fs.readFile)
+myReadFile('myfile.txt')
+  .then(file => console.log(file))
+  .catch(err => console.error(err))
+
+new Promise((resolve, reject) => {
+  collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }], (err, result) => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(result)
+    }
+  })
+})
+
+let myInsertMany = util.promisify(collection.insertMany)
+
+Promise.all([$.get(url1), $.get(url2)]).then(([result1, result2]) => {
+  console.log(result1, result2)
+})
+
+/**
+ * async/await
+ */
+
+async function fetchLuke() {
+  try {
+    let response1 = await fetch('https://swapi.co/api/people/')
+    let body1 = await response1.json()
+    console.log(body1)
+    let response2 = await fetch(body1.results[0].url)
+    let body2 = await response2.json()
+    console.log(body2)
+  } catch (err) {
+    console.error(err)
+  } finally {
+    console.warn('Cleanup')
+  }
+}
+
+async function whatever() {
+  let a = 1
+  let b = 2
+  let sum = a + b
+  let result = await $.get(url)
+
+  await new Promise(resolve => {
+    setTimeout(() => resolve('hello'), 1000)
+  })
+
+  console.log(result)
+}
+
+async function getAnswer() {
+  let [data1, data2] = await Promise.all([$.get(url1), $.get(url2)])
+  return {
+    answer1: data1,
+    answer2: data2,
+  }
+}
+
+getAnswer().then(obj => console.log(obj))
